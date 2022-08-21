23cd7a01c3ca20e6e9c09e66d4cdee3d27f706f7
NizarNasraoui1 <nizar.al.nasraoui@gmail.com>
2022-08-21 14:33:11 +0200

    contact tabs

---
+++ src/app/features/contacts/containers/contact-tabs/contact-tabs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-tabs',
+  templateUrl: './contact-tabs.component.html',
+  styleUrls: ['./contact-tabs.component.scss']
+})
+export class ContactTabsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
