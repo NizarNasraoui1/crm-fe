c85781be57f8d67ff5428492e4b277e5cd55337d
NizarNasraoui1 <nizar.al.nasraoui@gmail.com>
2022-09-03 02:04:11 +0200

    removing unused components 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,6 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppLayoutModule } from './layout/app.layout.module';
 import { NotfoundComponent } from './features/notfound/notfound.component'
-import { ProductService } from './demo/service/product.service';
-import { CountryService } from './demo/service/country.service';
-import { EventService } from './demo/service/event.service';
-import { IconService } from './demo/service/icon.service';
-import { NodeService } from './demo/service/node.service';
-import { PhotoService } from './demo/service/photo.service';
 import { authInterceptorProviders } from './core/_helpers/auth.interceptor';
 import { AuthService } from './core/_services/auth.service';
 
